370be249430d8fb4a4427176724b7b1b13ac8bb6
Deepak Agrawal <deepacks@gmail.com>
2018-03-01 18:05:35 +0000

    yang config in json working for bgp

--- yang/action_plugins/json_parser.py
+++ yang/action_plugins/json_parser.py
@@ -17,6 +17,7 @@ from ansible.module_utils.network.common.utils import to_list
 from ansible.module_utils.six import iteritems, string_types
 from ansible.module_utils._text import to_bytes, to_text
 from ansible.errors import AnsibleError, AnsibleUndefinedVariable, AnsibleFileNotFound
+from collections import OrderedDict
 
 try:
     from __main__ import display
@@ -28,6 +29,12 @@ def warning(msg):
     if C.ACTION_WARNINGS:
         display.warning(msg)
 
+openconfig_xmlns = {
+    "<bgp>" : "<bgp xmlns=\"http://openconfig.net/yang/bgp\">",
+    "<afi-safi-name>" : "<afi-safi-name xmlns:idx=\"http://openconfig.net/yang/bgp-types\">",
+    "ipv4-unicast" : "idx:ipv4-unicast",
+    "True"         : "true"
+}
 
 class ActionModule(ActionBase):
 
@@ -50,16 +57,33 @@ class ActionModule(ActionBase):
 
         with open(src, 'r') as f:
            json_config = f.read()
-           j_obj = json.loads(json_config)
+           j_obj = json.loads(json_config, object_pairs_hook=OrderedDict)
            config_xml = self._json_to_xml(j_obj)
-           q(config_xml)
+           config_xml_w_xmlns = self._add_openconfig_xmlns_to_config(config_xml,
+                   openconfig_xmlns)
+           config_xml_final = self._post_openconfig_parsing(config_xml_w_xmlns)
 
         with open(output_file, 'w') as f:
-            f.write(config_xml)
+            f.write(config_xml_final)
         
         #self.ds.update(task_vars)
         result['ansible_facts'] = self.facts
         return result
+
+    def _add_openconfig_xmlns_to_config(self, result_list, xmlns_dict):
+        for key in xmlns_dict:
+           result_list = result_list.replace(key, xmlns_dict[key])
+        
+        return (result_list)
+
+    def _post_openconfig_parsing(self, result_list, line_padding=""):
+        start_tag = "<config>\n"
+        end_tag   = "\n%s</%s>" % (line_padding, "config")
+        result_list = start_tag + result_list + end_tag
+        for i in range(0, 5):
+            q(result_list[i])
+        return (result_list)
+        
 
     def _json_to_xml(self, json_obj, line_padding=""):
         result_list = []
@@ -71,15 +95,16 @@ class ActionModule(ActionBase):
                result_list.append(self._json_to_xml(sub_elem, line_padding))
            return "\n".join(result_list)
 
-        if json_obj_type is dict:
+        #if json_obj_type is dict:
+        if json_obj_type is collections.OrderedDict:
            for tag_name in json_obj:
+               q(tag_name)
                sub_obj = json_obj[tag_name]
                result_list.append("%s<%s>" % (line_padding, tag_name))
                result_list.append(self._json_to_xml(sub_obj, "\t"+line_padding))
                result_list.append("%s</%s>" % (line_padding, tag_name))
-
            return "\n".join(result_list)
-
+           
         return "%s%s" % (line_padding, json_obj)
 
 
